c730f89ca22349154eccd0c46e618a385d7aa0a0
Antonio <antonio_ribeiro-n@estudante.sesisenai.org.br>
2026-04-27 09:47:59 -0300

    Changes

---
+++ script.js
@@ -0,0 +1,10 @@
+document.getElementById("formulario").addEventListener("submit", function(e) {
+
+e.preventDefault()
+
+let usuario = document.getElementById("usuario").value
+let senha = document.getElementById("senha").value
+
+
+
+})
